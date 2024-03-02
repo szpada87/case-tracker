50952e1fb66e280942a1105dadbee11d76bb4d49
szpada87 <szpada87@gmail.com>
2024-03-02 09:42:06 +0100

    added icon to error page

--- client/src/pages/Error/ErrorPage.tsx
+++ client/src/pages/Error/ErrorPage.tsx
@@ -1,4 +1,5 @@
 import { useRouteError } from "react-router-dom";
+import { FaBug } from 'react-icons/fa';
 import classes from "./Error.module.css";
 import { Card } from "../../components/Card/Card";
 
@@ -14,6 +15,7 @@ export default function ErrorPage() {
   return (
     <main id="error-page" className={classes.page_container}>
       <Card className={classes.card}>
+        <FaBug className={classes.icon} size="56" />
         <h1 className={classes.title}>Oops!</h1>
         <p className={classes.small}>Sorry, an unexpected error has occurred.</p>
         <p className={classes.description}>
