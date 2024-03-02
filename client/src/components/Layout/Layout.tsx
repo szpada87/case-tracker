c883e7a1ded802c0f45083aa500344c693aed36c
szpada87 <szpada87@gmail.com>
2024-03-02 11:49:54 +0100

    Refactored layout top navigation

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,8 +1,13 @@
 import { ReactNode } from 'react';
 import classes from "./Layout.module.css";
 import SideBar from '../SideBar/SideBar';
-import TopNavigation from '../TopNavigation/TopNavigation';
+import TopNavigation, { TopNavigationLink } from '../TopNavigation/TopNavigation';
 import Footer from '../Footer/Footer';
+import useUserMetadata from '../../hooks/useUserMetadata';
+import useAuthentication from '../../hooks/useAuthentication';
+import ThemeIcon from '../ThemeIcon/ThemeIcon';
+import { FaUserCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 type LayoutProps = {
     children: ReactNode
@@ -13,6 +18,9 @@ export const Layout = ({ children }: LayoutProps) => {
         <div className='flex'>
             <div className={classes.background}>
                 <div className={classes.content_container}>
+                    <TopNavigation>
+                        <ThemeIcon />
+                    </TopNavigation>
                     {children}
                     <Footer />
                 </div>
@@ -21,14 +29,28 @@ export const Layout = ({ children }: LayoutProps) => {
     )
 }
 
+const UserCircle = () => <FaUserCircle size='24' className={classes.user_icon} />;
+
 export const DashboardLayout = ({ children }: LayoutProps) => {
+    const { user } = useUserMetadata();
+    const { isAuthenticated } = useAuthentication();
     return (
         <div className='flex'>
             <SideBar />
             <div className={classes.background}>
                 <div className="ml-16">
                     <div className={classes.content_container}>
-                        <TopNavigation />
+                        <TopNavigation>
+                            {!isAuthenticated && <TopNavigationLink>Hello, there!</TopNavigationLink>}
+                            {user &&
+                                <TopNavigationLink>
+                                    <Link className='flex' to='/dashboard/profile' >
+                                        <UserCircle />
+                                        <span className={classes.user_name}>{user.name}</span>
+                                    </Link>
+                                </TopNavigationLink>}
+                            <ThemeIcon />
+                        </TopNavigation>
                         <div className={classes.content_list}>
                             {children}
                         </div>
--- client/src/components/TopNavigation/TopNavigation.tsx
+++ client/src/components/TopNavigation/TopNavigation.tsx
@@ -1,29 +1,30 @@
-import { FaHashtag, FaUserCircle } from 'react-icons/fa';
-import ThemeIcon from '../ThemeIcon/ThemeIcon';
+import { FaHashtag } from 'react-icons/fa';
 import classes from './TopNavigation.module.css'
-import useUserMetadata from '../../hooks/useUserMetadata';
-import { Link } from 'react-router-dom';
-import useAuthentication from '../../hooks/useAuthentication';
 
-const TopNavigation = () => {
-    const { user } = useUserMetadata();
-    const { isAuthenticated } = useAuthentication();
+import { ReactNode } from 'react';
 
+type TopNavigationProps = {
+    children?: ReactNode
+}
+
+const TopNavigation = ({ children }: TopNavigationProps) => {
     return (
         <div className={classes.top_navigation}>
             <FaHashtag size='20' className={classes.title_hashtag} />
             <h5 className={classes.title_text}>case-tracker</h5>
-            <ThemeIcon />
-            {!isAuthenticated && <div className={classes.top_navigation_link}>Hello, there!</div>}
-            {user &&
-                <Link className={classes.top_navigation_link} to='/dashboard/profile' >
-                    <UserCircle />
-                    <span className={classes.user_name}>{user.name}</span>
-                </Link>}
+            {children}
+           
         </div>
     );
 };
 
-const UserCircle = () => <FaUserCircle size='24' className={classes.top_navigation_icon} />;
-
 export default TopNavigation;
+
+type TopNavigationLinkProps = {
+    children: ReactNode
+}
+
+export const TopNavigationLink = ({ children }: TopNavigationLinkProps) => {
+    return <div className={classes.top_navigation_link}>{children}</div>
+
+}
